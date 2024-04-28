8f5e7e3f8f5b343a7685522983f811474a48e1be
Frykits <88166479+Frykits@users.noreply.github.com>
2024-04-27 22:33:18 -0300

    Update bubbleSort.go

--- bubbleSort/bubbleSort.go
+++ bubbleSort/bubbleSort.go
@@ -2,11 +2,12 @@ package main
 
 import "fmt"
 
+// Função que ordena um array de inteiros utilizando o algoritmo Bubble Sort
 func bubbleSort(arr []int) []int {
-	for i := 0; i < len(arr); i++ {
-		for j := 0; j < len(arr)-i-1; j++ {
-			if arr[j] > arr[j+1] {
-				arr[j], arr[j+1] = arr[j+1], arr[j]
+	for i := 0; i < len(arr); i++ { //quantidade de vezes que o array será percorrido
+		for j := 0; j < len(arr)-i-1; j++ { //percorre o array e compara os elementos adjacentes
+			if arr[j] > arr[j+1] { //se o elemento atual for maior que o próximo
+				arr[j], arr[j+1] = arr[j+1], arr[j] //troca os elementos de posição
 			}
 		}
 	}
@@ -14,7 +15,7 @@ func bubbleSort(arr []int) []int {
 }
 
 func main() {
-	arr := []int{64, 34, 25, 12, 22, 11, 90}
-	fmt.Println("Unsorted array is: ", arr)
-	fmt.Println("Sorted array is: ", bubbleSort(arr))
+	arr := []int{64, 34, 25, 12, 22, 11, 90}          //array de exemplo
+	fmt.Println("Unsorted array is: ", arr)           //array desordenado
+	fmt.Println("Sorted array is: ", bubbleSort(arr)) //array ordenado
 }
